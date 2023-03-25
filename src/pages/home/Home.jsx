994adf197e97f6d565b472c2afdba20b61418775
Sojib Mondol <mdsojibmandol@gmail.com>
2023-03-25 19:20:28 +0600

    Authentication added and 2 home components added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../components/banner/Banner';
+import OurProgram from '../../components/ourProgram/OurProgram';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <OurProgram/>
         </div>
     );
 };

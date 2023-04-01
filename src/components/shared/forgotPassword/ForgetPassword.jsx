8e59ad011c99cf7bbf9bf7670478ae625f71a8db
Sojib Mondol <mdsojibmandol@gmail.com>
2023-04-01 15:16:11 +0600

    forget password route added

---
+++ src/components/shared/forgotPassword/ForgetPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForgetPassword = () => {
+    return (
+        <div>
+            <h3>This is forget password compo........</h3>
+        </div>
+    );
+};
+
+export default ForgetPassword;

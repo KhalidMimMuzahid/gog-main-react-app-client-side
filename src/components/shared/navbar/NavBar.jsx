8d24f8c291b40a0b4b5bd172593dc87c980c191e
Sojib Mondol <mdsojibmandol@gmail.com>
2023-04-08 00:53:00 +0600

    updated logo added

--- src/components/shared/navbar/NavBar.jsx
+++ src/components/shared/navbar/NavBar.jsx
@@ -7,7 +7,7 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
-import logo from "../../../assets/images/final logo.png";
+import logo from "../../../assets/images/gog logo-0212.png";
 import { AuthContext } from "../../../context/AuthProvider";
 import { toast } from "react-hot-toast";
 import Modal from "react-bootstrap/Modal";

9b4cda4bbdb123a47fe026b965412a1da78d1eb4
Sojib Mondol <mdsojibmandol@gmail.com>
2023-04-08 00:12:55 +0600

    404 page added

--- src/components/shared/notFoundPage/NotFoundPage.jsx
+++ src/components/shared/notFoundPage/NotFoundPage.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import "./NotFoundPage.css";
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
     return (
         <div>
-            <h2>404 page not found back to <Link to='/'> home</Link> </h2>
+            
+            <div class="page-404">
+                <div class="outer">
+                    <div class="middle">
+                        <div class="inner">
+                            
+                            <div class="inner-circle"><i class="fa fa-cogs"></i><span>404</span></div>
+                            <span class="inner-status">Opps! Internal Server Error!</span>
+                            <span class="inner-detail">Unfortunately we're having trouble loading the page you are looking for. Please come back in a while.</span>
+                            <div className='text-center'>
+                                <Link to='/'><span><button className='btn btn-success'>Back to home</button></span></Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };

c95e79836dd2f4e27dbbed7bd36808b1790e095b
Sojib Mondol <mdsojibmandol@gmail.com>
2023-04-16 11:13:30 +0600

    changes the css for supper 30

---
+++ src/components/chatBot/ChatBot.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const steps = [
+    {
+        id: '0',
+        message: 'Hey Geek!',
+ 
+        // This calls the next id
+        // i.e. id 1 in this case
+        trigger: '1',
+    }, {
+        id: '1',
+ 
+        // This message appears in
+        // the bot chat bubble
+        message: 'Please write your username',
+        trigger: '2'
+    }, {
+        id: '2',
+ 
+        // Here we want the user
+        // to enter input
+        user: true,
+        trigger: '3',
+    }, {
+        id: '3',
+        message: " hi {previousValue}, how can I help you?",
+        trigger: 4
+    }, {
+        id: '4',
+        options: [
+             
+            // When we need to show a number of
+            // options to choose we create alist
+            // like this
+            { value: 1, label: 'View Courses' },
+            { value: 2, label: 'Read Articles' },
+ 
+        ],
+        end: true
+    }
+];
+
+const ChatBot = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ChatBot;

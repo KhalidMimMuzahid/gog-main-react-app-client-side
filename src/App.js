faae3fcd50aadb7cf7f1964934fbb1b009d870a3
Sojib Mondol <mdsojibmandol@gmail.com>
2023-04-18 13:58:08 +0600

    super 30 responsive

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div>
-        {/* <Chat/> */}
+        <Chat/>
         <RouterProvider router={router}></RouterProvider>
         <Toaster />
       </div>

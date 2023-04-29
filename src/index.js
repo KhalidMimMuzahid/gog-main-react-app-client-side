67874683fc2cff58c71e5498a381bacd39c09feb
Sojib Mondol <mdsojibmandol@gmail.com>
2023-04-29 22:10:10 +0600

    Added amplify

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthProvider from './context/AuthProvider';
-
+// importing AWS amplify
+import { Amplify } from 'aws-amplify';
+import awsConfig from './aws-exports'
 //TanStack query--------
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 const queryClient = new QueryClient();
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// aws config 
+Amplify.configure(awsConfig);
+
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>

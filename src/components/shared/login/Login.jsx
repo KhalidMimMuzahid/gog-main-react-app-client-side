2702b4b0f17f9e7c145d301b63dfa50e20af1db4
Sojib Mondol <mdsojibmandol@gmail.com>
2023-05-07 09:41:56 +0600

    FAQ added and refund policy updated and accordions updated

--- src/components/shared/login/Login.jsx
+++ src/components/shared/login/Login.jsx
@@ -286,7 +286,7 @@ const Login = () => {
                     onClick={handleGoogleSignIn}
                     style={{ width: "100%", borderRadius: "30px" }}
                   >
-                    <FcGoogle /> <span>CONTINUE WITH GOOGLExxxxxxxxxxxxxxxxxxxxxxx</span>
+                    <FcGoogle /> <span>CONTINUE WITH GOOGLE</span>
                   </button>
                 </div>
                 <div className="button-google-custom">

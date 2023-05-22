f0fd33d6b5d923753099f8b4a05c29b76c6c338c
JAYENDRA06 <jayendra.awasthi2020@vitstudent.ac.in>
2023-05-22 15:51:41 +0530

    resolve: design issues

--- src/pages/additional_pages/subComponents/faqComponents/AccordionItems.jsx
+++ src/pages/additional_pages/subComponents/faqComponents/AccordionItems.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Accordion } from "react-bootstrap";
+import style from "./Accordion.module.css"
 
 function AccordionItems({ item }) {
   return (
-    <Accordion.Item eventKey={item.id}>
-      <Accordion.Header>{item.question}</Accordion.Header>
-      <Accordion.Body className="acc-body">{item.answer}</Accordion.Body>
+    <Accordion.Item eventKey={item.id} className={style.acc_item}>
+      <Accordion.Header className={style.accordion_header}>{item.question}</Accordion.Header>
+      <Accordion.Body className={style.acc_body}>{item.answer}</Accordion.Body>
     </Accordion.Item>
   );
 }

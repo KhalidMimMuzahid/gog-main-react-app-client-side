cbe5bd2d835f4b471340f40d87c9ac3650a9654d
Sarwarhridoy4 <ssarwarhridoy4@gmail.com>
2023-05-22 16:05:54 +0600

    marquee modified

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -16,14 +16,14 @@ const TopBanner = ({ setShouldShowBanner }) => {
 
   return (
     <div onClick={handleBannerClick} className={`${style.bannerBody}`}>
-      <Marquee>
+      <Marquee speed={50} autoFill={true}>
       <div className="d-flex align-items-center justify-content-around p-2">
         {/* content */}
         <img src={chatGPT} alt="" />
         
           <h3>The Next gen Artificial Intelligence ChatGPT & AI Bard</h3>
         
-        <img src={chatGPT} alt="" />
+        {/* <img src={chatGPT} alt="" /> */}
         {/* content */}
         </div>
         </Marquee>
